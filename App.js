3d074bf04247651cef0321cd221e0de83450a515
Bartek Jaromin <83396630+barti9boy@users.noreply.github.com>
2022-01-17 17:34:46 +0100

    page changing setup

--- App.js
+++ App.js
@@ -1,66 +1,10 @@
-import React from 'react';
-import { useState } from 'react';
-import { StyleSheet, View, Button, TextInput } from 'react-native';
-import { WebView } from 'react-native-webview';
-import mapTemplate from './map-template';
+import React from "react";
+import { Text, View, Button, Alert, TouchableOpacity, TextInput } from 'react-native'
 
-export default function App() {
-  let webRef = undefined;
-  let [mapCenter, setMapCenter] = useState('-121.913, 37.361');
-
-  const onButtonPress = () => {
-    const [lng, lat] = mapCenter.split(",");
-    webRef.injectJavaScript(`map.setCenter([${parseFloat(lng)}, ${parseFloat(lat)}])`);
-  }
-
-  const handleMapEvent = (event) => {
-    setMapCenter(event.nativeEvent.data)
-  }
-
+const App = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.buttons}>
-        <TextInput 
-        style={styles.textInput}
-        onChangeText={setMapCenter}
-        value={mapCenter}></TextInput>
-        <Button title="Set Center" onPress={onButtonPress}></Button>
-      </View>
-      <WebView
-        ref={(r) => (webRef = r)}
-        onMessage={handleMapEvent}
-        style={styles.map}
-        originWhitelist={['*']}
-        source={{ html: mapTemplate }}
-      />
+    <View style={{flex: 1, justifyContent: 'center', alignItems: "center"}}>
+      <Button title="Log in" onPress={() => Alert.alert('hello')}></Button>
     </View>
-  );
+  )
 }
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    flex: 1
-  },
-  buttons: {
-    flexDirection: 'row',
-    height: '15%',
-    backgroundColor: '#fff',
-    color: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 12
-  },
-  textInput: {
-    height: 40,
-    width: "60%",
-    marginRight: 12,
-    paddingLeft: 5,
-    borderWidth: 1,
-  },
-  map: {
-    width: '100%',
-    height: '85%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
